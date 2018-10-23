623b428a2c950a9c5a75cc136fccadb6827f0c33
chinmayg <chinmay.ghute@prorigosoftware.com>
2018-10-23 18:24:54 +0530

    Some bug fixes

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var http = require('http');
-
 require('dotenv-extended').load();
 
-var myConfig = require('dotenv-extended').load();
+var http = require('http');
 var server = http.createServer(function (request, response) {
 
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end(myConfig.USERNAME);
-
+    response.end(process.env.USERNAME);
 });
 
 var port = process.env.PORT || 1337;

0521e2b39cdea043093db7a92f11110817a09292
chinmayghute <44160296+chinmayghute@users.noreply.github.com>
2018-10-23 18:19:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 
 
-var myConfig = require('dotenv-extended').load();
+
 var server = http.createServer(function (request, response) {
 
     response.writeHead(200, { "Content-Type": "text/plain" });

4aad325241926c9c5202639bafb1d770363a15b0
chinmayg <chinmay.ghute@prorigosoftware.com>
2018-10-24 12:15:34 +0530

    for new

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-
+require('dotenv-extended').load();
 const http = require('http');
 
 const server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("hello");
+    response.end(process.env.USER);
 });
-
+console.log(process.env.PORT);
 const port = process.env.PORT || 1337;
-
+console.log(port);
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);

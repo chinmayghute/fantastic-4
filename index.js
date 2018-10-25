7a30c38624dc335c570beb4ccd60d4f324e11b63
chinmayg <chinmay.ghute@prorigosoftware.com>
2018-10-25 11:35:13 +0530

    js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express');
+var app = express();
+var port = 3000;
 
-app.get('/', (req, res) => res.send("HELLO WORLD"))
+app.get('/', function (req, res) {
+    res.send("HELLO WORLD");
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, function () {
+    console.log("Example app listening on port: ", port, "!");
+});

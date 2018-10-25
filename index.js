068ae59bbe27c73f986d905941981508a5289da2
chinmayg <chinmay.ghute@prorigosoftware.com>
2018-10-25 11:26:09 +0530

    express

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
-var http = require('http');
+app.get('/', (req, res) => res.send("HELLO WORLD"))
 
-var server = http.createServer(function (request, response) {
-
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello World!");
-
-});
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
